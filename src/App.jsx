39bd3aabc447d6c562ad3182da6fcdad0ec2c9ec
Jose Lema <jose.lema.2310@gmail.com>
2024-03-21 21:34:11 -0700

    Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Card from './components/Card'
+// import Hero from './components/Hero'
+// import Card from './components/Card'
 
 
 function App() {
   return (
     <div>
         <Navbar />
-        <Hero />
+        {/* <Hero /> */}
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
-export default function Hero(){
-    return(
+// export default function Hero(){
+//     return(
         
-    )
-}
+//     )
+// }
