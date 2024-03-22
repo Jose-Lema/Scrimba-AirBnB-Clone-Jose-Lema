81cbf79ab02807cd0c5fc04ed95d8212cf8ae47e
Jose Lema <jose.lema.2310@gmail.com>
2024-03-22 15:33:28 -0700

    Finalized the Card system as well as adding badges on cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar'
-//import Hero from './components/Hero'
+import Hero from './components/Hero'
 import Card from './components/Card'
 import data from './data'
 
@@ -9,20 +9,17 @@ function App() {
   const cardElements = data.map( el => {
     return <Card 
       key = {el.id}
-      img = {el.coverImg}
-      rating = {el.stats.rating}
-      reviewCount = {el.stats.reviewCount}
-      location = {el.location}
-      title = {el.title}
-      price = {el.price}
+      {...el}
     />
   })
 
   return (
     <div>
         <Navbar />
-        {/* <Hero /> */}
-        {cardElements}
+        <Hero />
+        <section className='cards-list'>
+          {cardElements}
+        </section>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,23 @@
 import star from '../images/star.png'
 export default function Card(props){
+
+    let badgeText;
+    if (props.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.location == 'Online') {
+        badgeText = 'ONLINE'
+    }
+
     return(
         <div className='card'>
-            <img src={`src/images/${props.img}`} className='card--image' />
+            {badgeText && <div className='card--badge'>{badgeText}</div>}
+            <img src={`src/images/${props.coverImg}`} className='card--image' />
             <div className='card--stats'>
                 <img src={star} alt="Star" className='card--star' />
-                <span>{props.rating}</span>
-                <span className = 'gray'>({props.reviewCount}) •</span>
+                <span>{props.stats.rating}</span>
+                <span className = 'gray'>({props.stats.reviewCount}) •</span>
                 <span className = 'gray'>{props.location}</span>
-                <p>{props.title}</p>
+                <p className='card--title'>{props.title}</p>
                 <p><strong>From ${props.price}</strong> / person</p>
             </div>
         </div>        
