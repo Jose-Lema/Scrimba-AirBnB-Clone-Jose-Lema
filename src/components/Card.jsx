935be6a004b2e765a69c4fe6859b413da8fe5fb2
Jose Lema <jose.lema.2310@gmail.com>
2024-03-22 14:28:45 -0700

    First Card element created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,17 @@
-// export default function Card(){
-//     return(
-        
-//     )
-// }
+import katie from '../images/katie-zaferes.png';
+import star from '../images/star.png'
+export default function Card(){
+    return(
+        <div className='card'>
+            <img src={katie} alt="Katie Zaferes" className='card--image' />
+            <div className='card--stats'>
+                <img src={star} alt="Star" className='card--star' />
+                <span>5.0</span>
+                <span className = 'gray'>(6) •</span>
+                <span className = 'gray'>USA</span>
+                <p>Life Lessons with Katie Zaferes</p>
+                <p><strong>From $136</strong> / person</p>
+            </div>
+        </div>        
+    )
+}
