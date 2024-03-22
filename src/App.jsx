33d08ad638739d931e53b81a98a3a3bd2c4136ee
Jose Lema <jose.lema.2310@gmail.com>
2024-03-22 15:05:49 -0700

    Mapped out cards to be read from data file

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import './App.css'
 import Navbar from './components/Navbar'
 //import Hero from './components/Hero'
 import Card from './components/Card'
-
+import data from './data'
 
 function App() {
+
+  const cardElements = data.map( el => {
+    return <Card 
+      key = {el.id}
+      img = {el.coverImg}
+      rating = {el.stats.rating}
+      reviewCount = {el.stats.reviewCount}
+      location = {el.location}
+      title = {el.title}
+      price = {el.price}
+    />
+  })
+
   return (
     <div>
         <Navbar />
         {/* <Hero /> */}
-        <Card />
+        {cardElements}
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,15 @@
-import katie from '../images/katie-zaferes.png';
 import star from '../images/star.png'
-export default function Card(){
+export default function Card(props){
     return(
         <div className='card'>
-            <img src={katie} alt="Katie Zaferes" className='card--image' />
+            <img src={`src/images/${props.img}`} className='card--image' />
             <div className='card--stats'>
                 <img src={star} alt="Star" className='card--star' />
-                <span>5.0</span>
-                <span className = 'gray'>(6) •</span>
-                <span className = 'gray'>USA</span>
-                <p>Life Lessons with Katie Zaferes</p>
-                <p><strong>From $136</strong> / person</p>
+                <span>{props.rating}</span>
+                <span className = 'gray'>({props.reviewCount}) •</span>
+                <span className = 'gray'>{props.location}</span>
+                <p>{props.title}</p>
+                <p><strong>From ${props.price}</strong> / person</p>
             </div>
         </div>        
     )
